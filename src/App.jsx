6a867e33abab6fd92c86c1fa6232d310cfb4f897
Omaki-R <84137770+Omaki-R@users.noreply.github.com>
2021-05-23 22:00:51 +0900

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
+  // const contentStyle = {
+  //   color: "blue",
+  //   fontSize: "18px"
+  // };
   return (
     <>
-      <h1 style={{ color: "red" }}>こんちは</h1>
-      <p style={contentStyle}>makino</p>
+      <h1 style={{ color: "black" }}>こんちは!!</h1>
+      <ColorfulMessage color="blue">オゲンキデスカ？</ColorfulMessage>
+      <ColorfulMessage color="red">げんきです！！</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 // JSX記述のみでは不要？？？
 import ReactDom from "react-dom";
 import App from "./App.jsx";
+// import App from "./ColorfulMessage.jsx";
 
 ReactDom.render(<App />, document.getElementById("root"));
