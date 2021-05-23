e96313ae46f18014108427eecc0ffbc52d03d24e
Omaki-R <84137770+Omaki-R@users.noreply.github.com>
2021-05-23 22:54:10 +0900

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  // const contentStyle = {
-  //   color: "blue",
-  //   fontSize: "18px"
-  // };
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "black" }}>こんちは!!</h1>
       <ColorfulMessage color="blue">オゲンキデスカ？</ColorfulMessage>
       <ColorfulMessage color="red">げんきです！！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>CountUp</button>
+      <p>{num}</p>
     </>
   );
 };
